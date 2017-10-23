d37a919aecdc5236818c1ce7c55c3f13b3ce321e
Serios7 <Serios601@gmail.com>
2017-10-23 20:51:08 +1000

    Correction

--- list.py
+++ list.py
@@ -6,8 +6,7 @@
 # Если наибольших элементов несколько,
 # выведите индекс первого из них.
 
-list = list(input('Введите список чисел: '))
-
+list = input('Введите список чисел: ').split(',')
 num_max = max(list)
 print(num_max)
 print(list.index(num_max))
