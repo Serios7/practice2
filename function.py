54d700cec47f4800e671a6ecca93662ea7f5c826
Serios7 <Serios601@gmail.com>
2017-10-23 19:56:44 +1000

    Correction

--- function.py
+++ function.py
@@ -3,13 +3,19 @@
 # 5. Написать функцию avaranger,
 # которая принимает 1 аргумент - любое число и возвращается среднее арифметическое значение,
 # на основании текущего числа и предыдущих, которые были введены ранее.
-stock = []
-
-def Avaranger(num):
-    stock.append(num)
-    return sum(stock)/len(stock)
 
 
+def Avaranger():
+    stock = []
+    def inner_func(num):
+        stock.append(num)
+        return sum(stock)/len(stock)
+    return inner_func
+
+f = Avaranger()
 while True:
-    num = int(input('Введите число: '))
-    print(Avaranger(num))
+    num = input('Введите число или N для выхода: ')
+    if num == 'N':
+        break
+    else:
+        print(f(int(num)))
